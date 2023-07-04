4cb1d8053af13a7912ac1c141e39d28f7688e079
Cyan Yan <cyan_cg@outlook.com>
2023-07-05 01:28:46 +0800

    Modify the manage.py file to automatically import the required environment variables.

--- manage.py
+++ manage.py
@@ -2,11 +2,31 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import configparser
 
+# Get the path of the .env configuration file in the same directory.
+env_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env')
+
+def set_env():
+    """Set up environment variables."""
+    config = configparser.ConfigParser()
+    config.read(env_file_path)  
+    settings_module = config.get('DEFAULT', 'DJANGO_SETTINGS_MODULE')
+    db_name = config.get('DEFAULT', 'SOM_DB_NAME')
+    db_user = config.get('DEFAULT', 'SOM_DB_USER')
+    db_password = config.get('DEFAULT', 'SOM_DB_PASSWORD')
+    db_host = config.get('DEFAULT', 'SOM_DB_HOST')
+    db_port = config.get('DEFAULT', 'SOM_DB_PORT')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    os.environ.setdefault('SOM_DB_NAME', db_name)
+    os.environ.setdefault('SOM_DB_USER', db_user)
+    os.environ.setdefault('SOM_DB_PASSWORD', db_password)
+    os.environ.setdefault('SOM_DB_HOST', db_host)
+    os.environ.setdefault('SOM_DB_PORT', db_port)
 
 def main():
     """Run administrative tasks."""
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'som.settings')
+    set_env()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
